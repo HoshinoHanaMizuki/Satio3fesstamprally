97cb4084d5ddd8db5b682c4a1358ca5bb1bfe7c8
MasatomoIde <pakkuku1@gmail.com>
2024-09-23 13:34:47 +0900

    pageUpdate

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,81 @@
+"use client";
 import Image from "next/image";
+import { useState ,useEffect} from "react";
+type Festival ={
+  fesName:string;
+  releaseDate:string;
+  description:string[];
+  isLocked:boolean;
+}
 
+const festivalInfoArray:Festival[] = [
+  {fesName:"三財へそまつり",releaseDate:'2024-10-20',description:["A","B","C"],isLocked:false},
+  {fesName:"都於郡城址まつり",releaseDate:"2024-11-10",description:["A","B","C"],isLocked:true},
+  {fesName:"三納ちびっこ相撲",releaseDate:"2024-12-08",description:["A","B","C"],isLocked:true}
+]
+
+const fesImages:string[] = ["/image/MinouSumou.JPG","/image/MinouSumou.JPG","/image/MinouSumou.JPG"];
+const loadingImage:string = "/image/fesThumnail.png";
+const titleImage:string = "/image/eventTitle.jpg";
 export default function Home() {
+  const [isLoad,setIsLoad] = useState(true);
+  useEffect(() => {
+    // 4秒後にローダーを非表示にする
+    const timer = setTimeout(() => {
+      setIsLoad(false);
+    }, 2000);
+
+    // コンポーネントがアンマウントされたときにタイマーをクリア
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+    {isLoad ? (
+      <div className="loadingContent w-screen h-screen flex justify-center items-center">
+        <Image src={loadingImage} alt= "" layout="responsive" width={100} height={100}/>
+      </div>
+    ) : 
+    (
+      <>
+        <div className="contentscontainer w-screen h-screen mb-3 flex flex-col">
+          <div className="eventTitle text-lg text-center mb-5 w-screen ">
+            <Image src={titleImage} alt=""  layout="responsive" width={100} height={100}/>
+          </div>
+          <div className="festivalContainer grid  grid-cols-1 md:grid-cols-3 w-screen sm:flex-1 sm:h-auto h-screen gap-4 ">
+            <>
+              {festivalInfoArray.map((festival: Festival, index) => (
+                <div className={`${festival.fesName} h-screen flex flex-col`} key={index}>
+                  <div className="cardContainer relative h-3/5 m-5 bg-white rounded-md text-center items-center z-0">
+                    {festival.isLocked && (
+                      <div className="absolute inset-0 flex justify-center items-center bg-black bg-opacity-80 rounded-md z-10">
+                        <span className="text-white text-2xl font-bold">Coming Soon</span>
+                      </div>
+                    )}
+                    <div className="fesTitlecontainer relative  w-auto items-center h-4/5">
+                      <Image src={`${fesImages[index]}`} alt="" layout="fill" objectFit="cover" className="rounded-md"/>
+                      <div className="fesTitle text-center">{festival.fesName}</div>
+                    </div>
+                    <div className="descriptionTitle text-center">{festival.fesName}について</div>
+                    <div className="descriptionText text-center">{festival.description}</div>
+
+                    {/* ボタンの中央揃え */}
+                    <div className="linkButton flex justify-center items-center rounded-lg w-full mt-4">
+                      <button className="bg-blue-500 text-white py-2 px-4 rounded-lg">
+                        詳しくはこちら
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </>
+      
+    )}
+      
+    </>
   );
 }
+
